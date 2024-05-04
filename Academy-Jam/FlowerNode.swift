facd7db9aa981254196685b3d22d60b760cff027
educcamara <github0520@eduardo.ccamara.com>
2024-05-04 15:59:14 -0400

    Add isAlive e propriedade takeDamage

--- Academy-Jam/FlowerNode.swift
+++ Academy-Jam/FlowerNode.swift
@@ -11,7 +11,8 @@ import SpriteKit
 class FlowerNode: SKNode {
     let sprite: SKSpriteNode
     
-    private var _health: Int = 100
+    public var isAlive: Bool
+    private var _health: Int
     var health: Int {
         get {
             return self._health
@@ -28,9 +29,22 @@ class FlowerNode: SKNode {
     
     override init() {
         self.sprite = SKSpriteNode(color: .systemPink, size: .init(width: 30, height: 30))
+        self.isAlive = true
+        self._health = 100
         super.init()
         
         self.addChild(sprite)
+    }
+    
+    func takeDamage(damage: Int = 8) {
+        if !isAlive {return}
+        
+        self.health -= damage
+        print("Health: \(self.health)")
+        if self.health == 0 {
+            self.isAlive = false
+            self.sprite.color = .init(red: 0.3, green: 0, blue: 0, alpha: 1)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
